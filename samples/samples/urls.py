85d0d38cf712eac76f737dd5911a296c61ffcd10
BaLaMyt900 <57708651+BaLaMyt900@users.noreply.github.com>
2023-04-21 17:38:28 +0400

    Final

--- samples/samples/urls.py
+++ samples/samples/urls.py
@@ -5,6 +5,4 @@ from samples.views import *
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('pages/', include('django.contrib.flatpages.urls')),
-    path('about/', about_page),
-    path('', index_page)
 ]
--- samples/samples/views.py
+++ samples/samples/views.py
@@ -1,9 +1,1 @@
-from django.shortcuts import render
 
-
-def about_page(request):
-    return render(request, 'about.html')
-
-
-def index_page(request):
-    return render(request, 'index.html')
